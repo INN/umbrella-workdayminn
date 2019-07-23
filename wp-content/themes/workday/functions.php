0a5ad30da1593e85f5c73df04efb98db9ca2746f
Ben Keith <ben@benlk.com>
2019-07-23 17:03:18 -0400

    Initial pass on colors and styles.

--- wp-content/themes/workday/functions.php
+++ wp-content/themes/workday/functions.php
@@ -15,3 +15,20 @@ foreach ( $includes as $include ) {
 		require_once( get_stylesheet_directory() . $include );
 	}
 }
+
+/**
+ * Child styles include
+ */
+function workday_stylesheets() {
+	wp_dequeue_style( 'largo-child-styles' );
+	wp_deregister_style( 'largo-child-styles' );
+
+	$suffix = (LARGO_DEBUG) ? '.min' : '';
+	wp_enqueue_style(
+		'largo-child-styles',
+		get_stylesheet_directory_uri() . '/css/style' . $suffix . '.css',
+		array('largo-stylesheet'),
+		filemtime( get_stylesheet_directory() . '/css/style' . $suffix . '.css' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'workday_stylesheets', 20 );
