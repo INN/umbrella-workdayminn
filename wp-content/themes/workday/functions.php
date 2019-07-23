11af48531cc379b11657212db37d023c94a18d5c
Ben Keith <ben@benlk.com>
2019-07-22 21:55:47 -0400

    include the correct path of file

--- wp-content/themes/workday/functions.php
+++ wp-content/themes/workday/functions.php
@@ -8,7 +8,7 @@
  * Include files that should be included
  */
 $includes = array(
-	'inc/drupal-import.php',
+	'/inc/drupal-import.php',
 );
 foreach ( $includes as $include ) {
 	if ( 0 === validate_file( get_stylesheet_directory() . $include ) ) {
