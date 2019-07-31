e98398d25270f037caf90b5ecac8ffae57c031ef
Ben Keith <ben@benlk.com>
2019-07-31 16:29:51 -0400

    Add fonts, start first pass on using correct fonts and weights

--- wp-content/themes/workday/functions.php
+++ wp-content/themes/workday/functions.php
@@ -23,12 +23,19 @@ function workday_stylesheets() {
 	wp_dequeue_style( 'largo-child-styles' );
 	wp_deregister_style( 'largo-child-styles' );
 
+	// https://fonts.adobe.com/my_fonts?project_id=xio4whb#web_projects-section
+	wp_enqueue_style(
+		'workday-minnesota-adobe-fonts',
+		'https://use.typekit.net/xio4whb.css'
+	);
+
 	$suffix = (LARGO_DEBUG) ? '.min' : '';
 	wp_enqueue_style(
 		'largo-child-styles',
 		get_stylesheet_directory_uri() . '/css/style' . $suffix . '.css',
-		array('largo-stylesheet'),
+		array( 'largo-stylesheet', 'workday-minnesota-adobe-fonts' ),
 		filemtime( get_stylesheet_directory() . '/css/style' . $suffix . '.css' )
 	);
+
 }
 add_action( 'wp_enqueue_scripts', 'workday_stylesheets', 20 );
