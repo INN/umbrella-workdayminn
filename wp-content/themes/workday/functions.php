c81b2d0bfa3e69c8a4c78fc76a18be1ce122ca36
Ben Keith <ben@benlk.com>
2019-07-31 17:15:40 -0400

    Define colors according to official references, add Block Color compatibility.
    Colors are from the mockup referenced in https://github.com/INN/umbrella-workdayminn/issues/7#issuecomment-517013142

--- wp-content/themes/workday/functions.php
+++ wp-content/themes/workday/functions.php
@@ -10,6 +10,7 @@ define( 'SHOW_GLOBAL_NAV', FALSE );
  */
 $includes = array(
 	'/inc/drupal-import.php',
+	'/inc/block-color-palette.php',
 );
 foreach ( $includes as $include ) {
 	if ( 0 === validate_file( get_stylesheet_directory() . $include ) ) {
