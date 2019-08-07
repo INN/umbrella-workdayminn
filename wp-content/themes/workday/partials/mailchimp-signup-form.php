3a75e8c7119a7284d2ff39a61cf87d0be2969b28
Josh Darby <josh@inn.org>
2019-08-07 14:17:04 -0400

    Make sure MC alerts don't look wonky in the form

--- wp-content/themes/workday/partials/mailchimp-signup-form.php
+++ wp-content/themes/workday/partials/mailchimp-signup-form.php
@@ -2,11 +2,13 @@
 <div id="mc_embed_signup">
     <form action="https://umn.us3.list-manage.com/subscribe/post?u=8de5bb12c5759ae9b8ec629c2&amp;id=6b4407d0f5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div id="mc_embed_signup_scroll">
-            <div class="mc-field-group">
-                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email">
+            <div class="mc_field_container">    
+                <div class="mc-field-group">
+                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email">
+                </div>
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8de5bb12c5759ae9b8ec629c2_6b4407d0f5" tabindex="-1" value=""></div>
+                <div class="clear"><input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
             </div>
-            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8de5bb12c5759ae9b8ec629c2_6b4407d0f5" tabindex="-1" value=""></div>
-            <div class="clear"><input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
             <div id="mce-responses" class="clear">
                 <div class="response" id="mce-error-response" style="display:none"></div>
                 <div class="response" id="mce-success-response" style="display:none"></div>
